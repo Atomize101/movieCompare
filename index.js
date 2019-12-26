2c33fc1b7190b3ad79976c400be6086ac39a6e92
Chris Patrick <atomize101@gmail.com>
2019-12-26 15:31:21 -0500

    Search functionality to show movie poster and title

--- index.js
+++ index.js
@@ -6,25 +6,24 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data);
+    return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-    let timeoutId;
-    return (...args) => {
-        if(timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func.apply(null, args);
-        }, delay);
-    };
-};
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);    
 
-const onInput = (event) => {
-        fetchData(event.target.value);    
+    for(let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+             <img src="${movie.Poster}" />     
+             <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
 };
 input.addEventListener('input', debounce(onInput, 500)); 
 
