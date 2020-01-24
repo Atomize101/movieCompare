bddde5112b045d877fd8b8ced88fe6fd07894b70
Chris Patrick <atomize101@gmail.com>
2020-01-24 15:07:33 -0500

    Changing html for two inputs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
     renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
@@ -9,6 +8,7 @@ createAutoComplete({
             `
     },
     onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
         onMovieSelect(movie);
     },
     inputValue(movie) {
@@ -29,6 +29,16 @@ createAutoComplete({
         console.log(response.data.Search);
         return response.data.Search;
     }
+};
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete')
+});
+
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete')
 });
 
 const onMovieSelect = async movie => {
