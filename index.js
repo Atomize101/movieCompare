b11a2d51778f8ddc98d1ce61265e89af1f1406e3
Chris Patrick <atomize101@gmail.com>
2020-01-24 16:08:11 -0500

    Parsing values from movies

--- index.js
+++ index.js
@@ -76,7 +76,23 @@ const runComparison = () => {
 const movieTemplate = (movieDetail) => {
 
     const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
-    console.log(dollars);
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+
+    let count = 0;
+    const awards = movieDetail.Awards.split('').reduce((prev, word) => {
+        const value = parseInt(word);
+
+        if(isNaN(value)){
+            return prev;
+        } else {
+            return prev + value; 
+        }
+    }, 0);
+    console.log(awards);
+
+    console.log(dollars, metascore, imdbRating, imdbVotes)
 
     return `
         <article class="media">
