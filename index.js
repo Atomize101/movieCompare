39a78151444c0ddba387ec562bc433c7a12d60d6
Chris Patrick <atomize101@gmail.com>
2020-01-24 22:29:52 -0500

    Finishing compare logic

--- index.js
+++ index.js
@@ -75,7 +75,17 @@ const runComparison = () => {
 
     leftSideStats.forEach((leftStat, index) => {
         const rightStat = rightSideStats[index];
-        console.log(leftStat, rightStat);
+        
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+
+        if(rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary');
+            leftStat.classList.add('is-warning');
+        } else {
+            rightStat.classList.remove('is-primary');
+            rightStat.classList.add('is-warning');
+        }
     });
 };
 
