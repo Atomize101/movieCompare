ace7edbf7b86ad4b70392b5a7cd09e410e8b91a4
Chris Patrick <atomize101@gmail.com>
2020-01-24 22:18:46 -0500

    Important changes for comparison function

--- index.js
+++ index.js
@@ -70,7 +70,13 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-    console.log('Time to compare');
+    const leftSideStats = document.querySelectorAll('#left-summary .notification');
+    const rightSideStats = document.querySelectorAll('#right-summary .notification');
+
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+        console.log(leftStat, rightStat);
+    });
 };
 
 const movieTemplate = (movieDetail) => {
@@ -109,23 +115,23 @@ const movieTemplate = (movieDetail) => {
                 </div>
             </div>
         </article>
-        <article class="notification is-primary"?
+        <article data-value=${awards} class="notification is-primary"?
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary"?
+        <article data-value=${dollars} class="notification is-primary"?
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary"?
+        <article data-value=${metascore} class="notification is-primary"?
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary"?
+        <article data-value=${imdbRating} class="notification is-primary"?
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
-        <article class="notification is-primary"?
+        <article data-value=${imdbVotes} class="notification is-primary"?
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
